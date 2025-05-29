42797ff514740e4deb11b30c7a62178be2b5d790
UlfatJarin <richiulfat@gmail.com>
2025-05-30 00:35:59 +0600

    4th

--- src/Component/Asociate.jsx
+++ src/Component/Asociate.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Asociatecard from '../Layers/Asociatecard'
+import Container from '../Layers/Container'
 
 const Asociate = () => {
-  return (
-    <div>
-        <Asociatecard/>
-        
-    </div>
-  )
+    return (
+        <Container className='pt-[76px] pb-[108px] bg-black'>
+            <h2 className='font-semibold text-3xl text-white text-center mb-9 '>Asóciate</h2>
+            <div className='flex justify-between'>
+                <Asociatecard product='Socio Nuevo' price='$8.000' />
+                <Asociatecard product='Socio Pleno' price='$8.000' />
+                <Asociatecard product='Renovación' price='$6.500' />
+
+            </div>
+        </Container>
+    )
 }
 
 export default Asociate
