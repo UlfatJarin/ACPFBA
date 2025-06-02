11524530af7a550922f1bf2ca0439452e2cf8940
UlfatJarin <richiulfat@gmail.com>
2025-06-02 22:37:42 +0600

    gjh

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -8,7 +8,7 @@ import google from '../assets/google.png'
 
 const Login = () => {
     return (
-        <Container className='grid grid-cols-2 py-20 bg-black'>
+        <Container className='grid md:grid-cols-2 py-20 bg-black'>
             <div className='py-10 px-16 bg-[#272626]'>
                 <h2 className='h2 text-white mb-5'>Log in</h2>
                 <form className='flex flex-col gap-4' action="">
@@ -17,22 +17,22 @@ const Login = () => {
                     <label className='p1 text-white' htmlFor="">Password</label>
                     <input className='max-h-[40px] p-2 border border-white rounded-xl text-white bg-[#272626]' type="password" placeholder='' />
                     <div className='flex gap-4 items-center'>
-                        <input id='remember' className='w-[18px] h-[18px]' type="checkbox" />
+                        <input id='remember' className='md:w-[18px] md:h-[18px]' type="checkbox" />
                         <label className='p1 text-white' htmlFor="remember">Remember me</label>
                     </div>
                     <div className='flex gap-4 items-center'>
-                        <input id='Privacy' className='!w-[28px] !h-[28px]' type="checkbox" />
+                        <input id='Privacy' className='md:w-[28px] md:h-[28px]' type="checkbox" />
                         <label className='p1 text-white' htmlFor="Privacy">By continuing, you agree to the Terms of  Use and Privacy Policy</label>
                     </div>
-                    <button className=' btn py-1.5 px-[102px] border border-white rounded-xl mt-4  mx-auto '>Log in</button>
+                    <button className=' btn py-1.5  md:px-[102px] border border-white rounded-xl mt-4  mx-auto '>Log in</button>
                     <p className='font-medium text-lg text-white text-center'>Forget your password</p>
                     <div className='h-0.5 w-full bg-white mt-16 mb-7'></div>
                     <p className='text-center font-semibold text-xl text-white mb-4'>Or continue with</p>
                     <div className='flex justify-evenly'>
-                        <img className='w-[48px] h-[48px]' src={fb} alt="" />
-                        <img className='w-[48px] h-[48px]' src={apple} alt="" />
-                        <img className='w-[48px] h-[48px]' src={google} alt="" />
-                        <img className='w-[48px] h-[48px]' src={twtter} alt="" />
+                        <img className='w-[22px] md:w-[48px]' src={fb} alt="" />
+                        <img className='w-[22px] md:w-[48px]' src={apple} alt="" />
+                        <img className='w-[22px] md:w-[48px]' src={google} alt="" />
+                        <img className='w-[22px] md:w-[48px]' src={twtter} alt="" />
                     </div>
 
                 </form>
